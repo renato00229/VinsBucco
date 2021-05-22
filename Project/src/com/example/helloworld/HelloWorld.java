9b2df8caad8fa253fbc05159c24f295282d49096
Vinsjpg <per.vincenzo@live.it>
2021-05-22 09:38:45 +0200

    ciao

--- Project/src/com/example/helloworld/HelloWorld.java
+++ Project/src/com/example/helloworld/HelloWorld.java
@@ -2,7 +2,7 @@ package com.example.helloworld;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Mondo!");
     }
 }
 // questo è un commento
