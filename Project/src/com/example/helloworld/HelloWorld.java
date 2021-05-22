fdcd0dc98d416d89a432b1abb7abb0273ef93bbe
elena-3 <78790971+elena-3@users.noreply.github.com>
2021-05-22 09:55:47 +0200

    Aggiunto un commento

--- Project/src/com/example/helloworld/HelloWorld.java
+++ Project/src/com/example/helloworld/HelloWorld.java
@@ -3,6 +3,8 @@ package com.example.helloworld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Ciao Mondo!");
+        ciao prova = new ciao("red");
+        prova.print_c();
     }
 }
 
