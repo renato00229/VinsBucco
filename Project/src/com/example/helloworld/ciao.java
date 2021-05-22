b7ed815b3a5df655dea8ffb499e8b82146bf486f
Vinsjpg <per.vincenzo@live.it>
2021-05-22 10:31:17 +0200

    ehi

--- Project/src/com/example/helloworld/ciao.java
+++ Project/src/com/example/helloworld/ciao.java
@@ -3,13 +3,4 @@ package com.example.helloworld;
 public class ciao {
     public String color;
 
-    public ciao(String color){
-        this.color = color;
-
-    }
-
-    public void print_c (){
-        System.out.println(color);
-    }
-
 }
