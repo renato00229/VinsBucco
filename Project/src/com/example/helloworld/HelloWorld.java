23e809c5ef897022e8468cd8a8600a861c1c4fc3
renato00229 <renatobelmonte.rb@gmail.com>
2021-05-22 09:39:41 +0200

    Progetto di prova

--- Project/src/com/example/helloworld/HelloWorld.java
+++ Project/src/com/example/helloworld/HelloWorld.java
@@ -2,7 +2,7 @@ package com.example.helloworld;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello Mondo!");
+        System.out.println("Ciao Mondo!");
     }
 }
 // questo è un commento
